ddd0b253ce95be543e7934f5b0554c1f33fd84dd
nickm144 <46693174+nickm144@users.noreply.github.com>
2019-01-17 09:41:24 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nick Marxen
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
