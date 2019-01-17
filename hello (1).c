b5b1682c9bd8bd01a609801554d97039c109b5f3
nickm144 <46693174+nickm144@users.noreply.github.com>
2019-01-17 10:32:23 -0600

    Create hello (1).c

---
+++ hello (1).c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ahmed Albalushi
+ * Date: 2010/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Electrical Engineering!\n");
+  printf("Nick Marxen\n");
+  printf("Finance\n");
+
+  return 0;
+}
